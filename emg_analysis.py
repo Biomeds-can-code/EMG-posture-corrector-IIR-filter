8844c07832a0ee84c4db0a70dd5821c95a9c1dbc
SoFia2401 <sofiahgelado@gmail.com>
2021-12-26 15:44:39 +0000

    emg analysis with added comments

--- emg_analysis.py
+++ emg_analysis.py
@@ -11,62 +11,128 @@ import iir_filter
 import numpy as np
 from scipy import signal
 import os
+#import alert
+
 
 class emg_analysis:
+
+    """
+    This class contains the functions to perform the analysis of an input EMG signal. Intializing the class sets the threshold and sampling frequency of the input signal. Motion of the upper trunk is performed via a match filter. The match filter is a bandpass filter which lets through the frequency of the peak resulting from the extension or flexion of the muscles in the lower back. The result of the match filter is then translated into single detections via the implementation of heuristics in the detector function. 
+    """
     
     def __init__(self):
-        
-        self.threshold=10e-17
-        self.fs=250
-        self.i=0
     
-    def match_filter(self,data01):
+        """
+        Constructor function.
+        """
+        
+        self.threshold=1e-17 #defined threshold 
+        self.fs=250
 
-        # create a 2nd order order bandpass filter
+    def match_filter(self,data):
+    
+        """
+        The match filter allows the signal at frequencies equal to the frequency of the peak due to the flexion and extension of the trunk through.
+    
+        Arguments
+        ---------
+    
+        data01: Filtered data from the initial filtering step 
+    
+    
+        Returns 
+        -------
+    
+        y3: Rectified signal containing data for detection 
+    
+        
+        """
+
+        # create a 2nd order order bandpass filter that let's through frequency of motion peaks
         f1 = 0.5
         f2 = 1.5
         sos3 = signal.butter(3, [f1/self.fs*2.0, f2/self.fs*2.0 ], 'bandpass',output='sos')
-        b,a = signal.butter(3, [f1/self.fs*2.0, f2/self.fs*2.0 ], 'bandpass')
         iir2 = iir_filter.IIR_filter(sos3)
         
         y3 = []
-        y3= iir2.filter(data01)
+        y3= iir2.filter(data)
             
-        y3=y3*y3
+        y3=y3*y3 #improve signal to noise ratio of detection 
  
         return y3
         
 
-    def detector(self,data,start_slouch,state):
+    def detector(self,data,start_motion,state,start_time):
     
-    #state=1 slouching, state=0 not slouching 
+        """
+        The detector function uses the output of the match filter and a defined threshold to notify the user if they are slouching or in a correct position. 
+    
+        Arguments 
+        ---------
+    
+        data: data after initial filtering
+        start_motion: time at which the last recorded slouching/straightening of the trunk was detected
+        state: current state (state=1 slouching, state=0 not slouching)
+        start_time: time at which the program started 
+    
+    
+        Returns
+        -------
+    
+        start_motion: updated time of last recorded motion (if one was detected)
+        state: updated current state (if there was a change)
+    
+        """
 
-        ynew=self.match_filter(data)
-        time_since_slouch=time.time()-start_slouch
+
+        ynew=self.match_filter(data) #match filter output
+        time_since_slouch=time.time()-start_motion #calculate time since last detected change in trunk position 
      
-        minlen=3 #Minimum length of motion
+        minlen=3 #Minimum length of motion in seconds
         
-        if ynew>self.threshold and time_since_slouch>minlen:
-        
-            if state==0:
-                print('Stop slouching!')
-                print(datetime.datetime.now().time())
-                state=1
-                start_slouch=time.time()
-                os.system('spd-say "Stop slouching"')
-                
-            else:
-                print('Good job! Keep it up!')
-                print(datetime.datetime.now().time())
-                state=0
-                start_slouch=time.time()
-                os.system('spd-say "Good job"')
-
-
-        else:
-          pass
+        if time.time()-start_time<5: #Don't detect anything in the first 5 seconds to let filter settle
+            pass 
             
-        return start_slouch, state
+        else:
+        
+            if ynew>self.threshold and time_since_slouch>minlen: #threshold reached and sufficiend time between last detected trunk motion
+                if state==0: #was in not slouched state
+                
+                    #alert printed in terminal 
+                    print('Stop slouching!')
+                    print(datetime.datetime.now().time())
+                    
+                    #audible alert
+                    os.system('spd-say "Stop slouching"')
+                    
+                    #alert printed on window
+                    #alert.runWindow('alert')
+                    
+                    state=1 #change state to slouching
+                    start_motion=time.time() #record time of slouching
+                   
+		        
+                else: #was in slouched state
+                
+                    #alert printer in terminal 
+                    print('Good job! Keep it up!')
+                    print(datetime.datetime.now().time())
+                    
+                    #audible alert 
+                    os.system('spd-say "Good job"')
+                    
+                    #alert printed on window 
+                    #alert.runWindow('main')
+                    
+                    state=0 #change state to not slouched 
+                    start_motion=time.time() #record time of motion 
+                    
+
+
+            else:
+                pass
+            
+        return start_motion, state
 
         
 
