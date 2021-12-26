046beb9867f4cfd1a7eb4dc9da2e63bd3c5a829e
SoFia2401 <sofiahgelado@gmail.com>
2021-12-26 15:43:14 +0000

    new version of realtime.py with filtering for ECG artefacts

--- realtime.py
+++ realtime.py
@@ -9,7 +9,7 @@ from scipy import signal
 import threading
 from time import sleep
 import sys
-
+import time
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtGui
 
@@ -55,39 +55,48 @@ class QtPanningPlot:
 
 def getDataThread(qtPanningPlot1,qtPanningPlot2):
 
-    #Filtering for 50Hz and DC
+    #Filtering for 50Hz
     f0 = 48.0
     f1 = 52.0
     fs=250
 
     sos1 = signal.butter(3, [f0/(fs/2), f1/(fs/2)], 'bandstop', output='sos')
-        
-    f2 = 0.25
-    f3 = 124
-    sos2 = signal.butter(3, [f2/(fs/2), f3/(fs/2)], 'bandpass', output='sos')
-        
+    
+    #Filtering for DC 
+    
+    f2= 0.25
+  
+    sos2= signal.butter(3, [f2/(fs/2)], 'highpass', output='sos')
+            
+    #Filtering for ECG artefact
+    
+    f3= 5
+    f4= 30
+    
+    sos3= signal.butter(3, [f3/(fs/2), f4/(fs/2)], 'bandstop', output='sos')
+     
     iir1 = iir_filter.IIR_filter(sos1)
     iir2 = iir_filter.IIR_filter(sos2)
+    iir3 = iir_filter.IIR_filter(sos3)
         
-
-    global data 
-    i=0
-    last=0
-    analysis=emg_analysis.emg_analysis()
-    start_slouch=0
-    state=0
+    analysis = emg_analysis.emg_analysis()
+    start_motion = 0 #intialize time of detected slouch/correction of position
+    state = 0 #start in not slouched state
+    start_time = time.time() #time at which program started
     
     while running:
         # loop as fast as we can to empty the kernel buffer
         while c.hasSampleAvailable():
             sample = c.getSampleFromBuffer()
             sample=sample[ch1]
-            sample2= iir1.filter(iir2.filter(sample))
-            sample22=analysis.match_filter(sample2)
+            sample2= iir1.filter(iir2.filter(iir3.filter(sample)))
+            #sample22=analysis.match_filter(sample2)
             qtPanningPlot1.addData(sample)
-            qtPanningPlot2.addData(sample22)
-            data.append(sample)
-            [start_slouch,state]=analysis.detector(sample2,start_slouch,state)
+            qtPanningPlot2.addData(sample2)
+            
+            #Perform detection 
+            [start_motion,state]=analysis.detector(sample2,start_motion,state,start_time)
+
         # let Python do other stuff and sleep a bit
         sleep(0.1)
 
@@ -98,14 +107,11 @@ if not c:
     print("No Attys connected and/or paired")
     sys.exit()
 
-data=[]
-
-
-# Let's create two instances of plot windows
+# Instance of unfiltered and filtered data windows 
 qtPanningPlot1 = QtPanningPlot("Unfiltered")
 qtPanningPlot2 = QtPanningPlot("Filtered")
 
-# create a thread which gets the data from the USB-DUX
+# create a thread which gets the data from the Attys
 t = threading.Thread(target=getDataThread,args=(qtPanningPlot1,qtPanningPlot2,))
 
 # start data acquisition
